a2f651d870cb2dca4fe1af52c8febe2e5eb8a3f3
filippo <illiserf@gmail.com>
2018-12-05 17:20:32 +0100

    LoginPage.js updated with index.js

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -2,3 +2,77 @@ import React, { Component } from "react";
 import axios from "axios";
 import { Redirect } from "react-router-dom";
 
+class LoginPage extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        email: "",
+        originalPassword: "",
+      };
+    }
+  
+    genericSync(event) {
+      const { name, value } = event.target;
+      this.setState({ [name]: value });
+    }
+  
+    handleSubmit(event) {
+      event.preventDefault();
+  
+      axios
+        .post("http://localhost:5555/api/login", 
+        this.state,
+        {withCredentials: true},
+        )
+        .then(response => {
+          console.log("Login Page", response.data);
+          const { userDoc } = response.data;
+          // send "userDoc" to the App.js function that changes "currentUser"
+          this.props.onUserChange(userDoc);
+        })
+        .catch(err => {
+          console.log("Login Page ERROR", err);
+          alert("Sorry! Something went wrong.");
+        });
+    }
+  
+  
+    render() {
+        // check currentUser (received from App.js)
+      if (this.props.currentUser) {
+        return <Redirect to="/" />;
+      }
+      return (
+        <section className="LoginPage">
+          <h2>Log In</h2>
+          <form onSubmit={event => this.handleSubmit(event)}>
+            <label>
+              Email:
+              <input
+                value={this.state.email}
+                onChange={event => this.genericSync(event)}
+                type="email"
+                name="email"
+                placeholder="rey@jedi.com"
+              />
+            </label>
+            <label>
+              Password:
+              <input
+                value={this.state.originalPassword}
+                onChange={event => this.genericSync(event)}
+                type="password"
+                name="originalPassword"
+                placeholder="rey@jedi.com"
+              />
+            </label>
+            <button>Log In</button>
+          </form>
+        </section>
+      );
+    }
+  }
+  
+  export default LoginPage;
+  
