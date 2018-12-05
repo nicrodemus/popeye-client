8486736feba0baba316c0f8ae207a2be68a7ecbb
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2018-12-05 19:34:24 +0100

    bahh

--- src/components/SignupPage.js
+++ src/components/SignupPage.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./SignupPage.css";
 
 class SignupPage extends Component {
   constructor(props) {
@@ -51,66 +52,66 @@ class SignupPage extends Component {
 
     return (
       <section className="SignupPage">
-        <h2>Sign Up</h2>
+        <h2>Create an Account</h2>
+        <div className="SignupDiv">
+          <form
+            className="SignupForm"
+            onSubmit={event => this.handleSubmit(event)}
+          >
+            <label>
+              <input
+                size="50"
+                value={this.state.email}
+                onChange={event => this.genericSync(event)}
+                type="email"
+                name="email"
+                placeholder="Email Adress"
+              />
+            </label>
 
-        <form onSubmit={event => this.handleSubmit(event)}>
-          <label>
-            Email:
-            <input
-              value={this.state.email}
-              onChange={event => this.genericSync(event)}
-              type="email"
-              name="email"
-              placeholder="Rey@jedi.com"
-            />
-          </label>
+            <label>
+              <input
+                value={this.state.name}
+                onChange={event => this.genericSync(event)}
+                type="text"
+                name="name"
+                placeholder="Name"
+              />
+            </label>
 
-          <label>
-            Name:
-            <input
-              value={this.state.name}
-              onChange={event => this.genericSync(event)}
-              type="text"
-              name="name"
-              placeholder="Rey"
-            />
-          </label>
+            <label>
+              <input
+                value={this.state.surname}
+                onChange={event => this.genericSync(event)}
+                type="text"
+                name="surname"
+                placeholder="Surname"
+              />
+            </label>
 
-          <label>
-            Surname:
-            <input
-              value={this.state.surname}
-              onChange={event => this.genericSync(event)}
-              type="text"
-              name="surname"
-              placeholder="Rey"
-            />
-          </label>
+            <label>
+              <input
+                value={this.state.originalPassword}
+                onChange={event => this.genericSync(event)}
+                type="password"
+                name="originalPassword"
+                placeholder="Password"
+              />
+            </label>
 
-          <label>
-            Password:
-            <input
-              value={this.state.originalPassword}
-              onChange={event => this.genericSync(event)}
-              type="password"
-              name="originalPassword"
-              placeholder="****"
-            />
-          </label>
+            <label>
+              <input
+                value={this.state.phoneNumber}
+                onChange={event => this.genericSync(event)}
+                type="number"
+                name="phoneNumber"
+                placeholder="Phone Number"
+              />
+            </label>
 
-          <label>
-            PhoneNumber:
-            <input
-              value={this.state.phoneNumber}
-              onChange={event => this.genericSync(event)}
-              type="number"
-              name="phoneNumber"
-              placeholder="type your number"
-            />
-          </label>
-
-          <button>Sign Up</button>
-        </form>
+            <button>Sign Up</button>
+          </form>
+        </div>
       </section>
     );
   }
