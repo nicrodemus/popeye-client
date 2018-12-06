871a4f5efcbd5637353e15302a296883f419c3fd
Nicolas Pe <perrot.nic@gmail.com>
2018-12-06 17:14:09 +0100

    created components

---
+++ src/components/SearchResult.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import { Switch, Route, NavLink } from "react-router-dom";
+class SearchResult extends Component {
+  render() {
+    return (
+      <section className="SearResult">
+
+      </section>
+    );
+  }
+}
+
+export default SearchResult;
