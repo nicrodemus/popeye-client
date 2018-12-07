f44b3def445671f76586acab5b6279f85ef90ffb
Nicolas Pe <perrot.nic@gmail.com>
2018-12-07 12:22:07 +0100

    moved components into folders

---
+++ src/components/LoginPage/LoginPage.js
@@ -0,0 +1,98 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect, NavLink } from "react-router-dom";
+import "./LoginPage.css";
+
+class LoginPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      originalPassword: ""
+    };
+  }
+
+  genericSync(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:5555/api/login", this.state, {
+        withCredentials: true
+      })
+      .then(response => {
+        console.log("Login Page", response.data);
+        const { userDoc } = response.data;
+        // send "userDoc" to the App.js function that changes "currentUser"
+        this.props.onUserChange(userDoc);
+      })
+      .catch(err => {
+        console.log("Login Page ERROR", err);
+        alert("Sorry! Something went wrong. Login");
+      });
+  }
+
+  render() {
+    // check currentUser (received from App.js)
+    if (this.props.currentUser) {
+      return <Redirect to="/" />;
+    }
+    return (
+      <section className="LoginPage">
+        <div>
+          <div className="login-div">
+            <h2>Log in to your account</h2>
+
+            <form
+              className="login-form pad-40"
+              onSubmit={event => this.handleSubmit(event)}
+            >
+              <label>
+                <input
+                  value={this.state.email}
+                  onChange={event => this.genericSync(event)}
+                  type="email"
+                  name="email"
+                  placeholder="rey@jedi.com"
+                />
+              </label>
+
+              <label>
+                <input
+                  className="margin-top-20"
+                  value={this.state.originalPassword}
+                  onChange={event => this.genericSync(event)}
+                  type="password"
+                  name="originalPassword"
+                  placeholder="rey@jedi.com"
+                />
+              </label>
+
+              <button className="button-container margin-top-20">
+                <p>Log In</p>
+              </button>
+            </form>
+
+            <p>
+              New to Popeye?
+              <NavLink to="/signup-page"> Sign Up</NavLink>
+            </p>
+          </div>
+
+          <div>
+            <NavLink className="margin-top-20 flex j-cont-center" to="/reset-password">
+              Forgot your Password?
+            </NavLink>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default LoginPage;
---
+++ src/components/SignupPage/SignupPage.js
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+import axios from "axios";
+import "./SignupPage.css";
+import { NavLink } from "react-router-dom";
+
+class SignupPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      name: "",
+      surname: "",
+      originalPassword: "",
+      phoneNumber: "",
+      currentUser: null
+    };
+  }
+
+  genericSync(event) {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+
+    axios
+      .post("http://localhost:5555/api/signup", this.state, {
+        withCredentials: true
+      })
+      .then(response => {
+        console.log("Signup Page", response.data);
+        const { userDoc } = response.data;
+        // send "userDoc" to the App.js function that changes "currentUser"
+        this.props.onUserChange(userDoc);
+      })
+      .catch(err => {
+        console.log("Signup Page Error", err);
+        alert("Sorry! Something went wrong. Signup");
+      });
+  }
+
+  render() {
+    if (this.props.currentUser) {
+      return (
+        <section className="SignupPage">
+          <h2>You are signed up!</h2>
+          <p>Welcome, {this.props.currentUser.name}!</p>
+        </section>
+      );
+    }
+
+    return (
+      <section className="SignupPage">
+        <div className="signup-div">
+        <h2>Create an Account</h2>
+        
+          <form
+            className="signup-form pad-40"
+            onSubmit={event => this.handleSubmit(event)}
+          >
+
+            <label>
+              <input
+                className=""
+                value={this.state.name}
+                onChange={event => this.genericSync(event)}
+                type="text"
+                name="name"
+                placeholder="Name"
+              />
+            </label>
+
+            <label>
+              <input
+                className="margin-top-20"
+                value={this.state.surname}
+                onChange={event => this.genericSync(event)}
+                type="text"
+                name="surname"
+                placeholder="Surname"
+              />
+            </label>
+
+            <label>
+              <input
+                className="margin-top-20"
+                value={this.state.phoneNumber}
+                onChange={event => this.genericSync(event)}
+                type="number"
+                name="phoneNumber"
+                placeholder="Phone Number"
+              />
+            </label>
+
+            <label>
+              <input
+               className="margin-top-20"
+                value={this.state.email}
+                onChange={event => this.genericSync(event)}
+                type="email"
+                name="email"
+                placeholder="Email Adress"
+              />
+            </label>
+
+            <label>
+              <input
+                className="margin-top-20"
+                value={this.state.originalPassword}
+                onChange={event => this.genericSync(event)}
+                type="password"
+                name="originalPassword"
+                placeholder="Password"
+              />
+            </label>
+
+            <label>
+              <input
+                className="margin-top-20"
+                value={this.state.originalPassword}
+                onChange={event => this.genericSync(event)}
+                type="password"
+                name="originalPassword"
+                placeholder="Repeat Password"
+              />
+            </label>
+
+            <button className="button-container margin-top-20">
+              <p>Sign Up</p>
+            </button>
+          </form>
+
+          <p>
+            Already a client?
+            <NavLink to="/login-page"> Log In</NavLink>
+          </p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default SignupPage;
