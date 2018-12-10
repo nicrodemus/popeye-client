b8d478809703a13768b2be0a484c3aabfc51b7cc
Niccolò Boscolo <niccolo.boscolo88@icloud.com>
2018-12-10 18:49:43 +0100

    map quite stable

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -2,30 +2,60 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 
 export class MapContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showingInfoWindow: true
+    };
+  }
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     const { coordinates } = this.props;
+    console.log("cooooooooooordinates", coordinates);
+    const lng = coordinates[0];
+    const lat = coordinates[1];
     return (
       <Map
         google={this.props.google}
         style={{ width: "60%", height: "60%", position: "relative" }}
         initialCenter={{
-          lat: 48.856614,
-          lng: 2.3522219000000177
+          lat: 48.864716,
+          lng: 2.349014
         }}
         className={"map"}
         zoom={12}
       >
         <Marker
-          title={"Jean-Michel Apeuprès"}
+          title={"The marker`s title will appear as a tooltip."}
           name={"SOMA"}
-          position={{ lat: coordinates[0], lng: coordinates[1] }}
+          position={{ lat: lat, lng: lng }}
+          onClick={this.onMarkerClick}
         />
-        {/* <Marker onClick={this.onMarkerClick} name={"Current location"} /> */}
 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          {/* <div>
-            <h1>{this.state.selectedPlace.name}</h1>
-          </div> */}
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h1>{this.props.fullName}</h1>
+          </div>
         </InfoWindow>
       </Map>
     );
